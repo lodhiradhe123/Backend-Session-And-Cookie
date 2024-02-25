4e2e705b2d560714e08ab6f76cf457c6c95732d1
Radheshyam lodhi <radheshyamlodhi48@gmail.com>
2024-02-25 15:02:34 +0530

    cookie handling => create, read and delete

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,19 @@
 var express = require("express");
 var router = express.Router();
-const userModel = require("./users");
 
 router.get("/", function (req, res) {
-  req.session.ban = true;
+  res.cookie("age", 24);
   res.render("index");
 });
-router.get("/checkban", function (req, res) {
-  if (req.session.ban === true) {
-    res.send("you are banned");
-  } else {
-    res.send("you are not ban");
-  }
+
+router.get("/read", function (req, res) {
+  console.log("req.cookies.age");
+  res.send("check");
 });
-router.get("/removeban", function (req, res) {
-  req.session.destroy(function (err) {
-    if (err) throw err;
-    res.send("ban removed");
-  });
+
+router.get("/delete", function (req, res) {
+  res.clearCookie("age");
+  res.send("cookie clear ho gai ");
 });
 
 module.exports = router;
